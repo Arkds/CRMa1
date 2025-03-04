c0b7d4983218bd80d888fddc6b881bae7b95ea6d
Arkds <arnolflo23@gmail.com>
2025-03-04 14:48:12 -0500

    recursos de cdn a local

--- header.php
+++ header.php
@@ -4,20 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="src\bootstrapcss.css" rel="stylesheet">
+    <link href="src\datatablescss.css" rel="stylesheet">
 
-    <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
+    <script src="src\jquery.js"></script>
+    <script src="src\datatablesjs.js"></script>
+    <script src="src\chartjs.js"></script>
+    <script src="src\chartplugin.js"></script>
+    
 
-
-
-
-
-    <title>Dashboard</title>
+    <title>CRM</title>
 </head>
 
 <body>
